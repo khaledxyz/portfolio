5dafdbedd8a7b903c2153bdafefc86992e4022ac
khaledxyz <khaledhalloua@outlook.com>
2025-12-16 10:25:41 +0100

    update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,21 @@
 import { Fragment } from "react/jsx-runtime";
 
 import { AboutSection } from "./_components/about-section";
+import { CtaSection } from "./_components/cta-section";
 import { FeaturesSection } from "./_components/features-section";
+import { HeroSection } from "./_components/hero-section";
 import { ProjectsSection } from "./_components/projects-section";
 import { TechnologiesSection } from "./_components/technologies-section";
 
 export default function Page() {
   return (
     <Fragment>
+      <HeroSection />
       <AboutSection />
       <ProjectsSection />
       <TechnologiesSection />
       <FeaturesSection />
+      <CtaSection />
     </Fragment>
   );
 }
