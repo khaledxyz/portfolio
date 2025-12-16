9df5aa5cf803667339bc751ef31f6065108cfc31
khaledxyz <khaledhalloua@outlook.com>
2025-12-16 10:24:01 +0100

    Create hero-section.tsx

---
+++ src/app/(home)/_components/hero-section.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+import { ArrowRight01FreeIcons } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+export function HeroSection() {
+  return (
+    <section>
+      <div className="container space-y-5">
+        <Badge variant="secondary">Check source code on Github!</Badge>
+        <h1 className="font-bold text-5xl md:text-6xl">
+          Engineering products that scale beyond orbit
+        </h1>
+        <p className="max-w-2xl text-muted-foreground">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga impedit
+          nam voluptate fugiat maiores autem esse. Commodi, ratione aliquam.
+          Perferendis?
+        </p>
+        <Button asChild size="lg">
+          <Link href="#projects">
+            See My Work
+            <HugeiconsIcon icon={ArrowRight01FreeIcons} strokeWidth={2} />
+          </Link>
+        </Button>
+      </div>
+    </section>
+  );
+}
