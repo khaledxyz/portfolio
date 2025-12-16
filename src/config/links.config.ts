359de57a0db5af94e320b7f180e6ebbb1036179e
khaledxyz <khaledhalloua@outlook.com>
2025-12-16 10:25:35 +0100

    Create links.config.ts

---
+++ src/config/links.config.ts
@@ -0,0 +1,19 @@
+export const links = [
+  {
+    label: "About",
+    href: "#about",
+  },
+
+  {
+    label: "Projects",
+    href: "#projects",
+  },
+  {
+    label: "Technologies",
+    href: "#technologies",
+  },
+  {
+    label: "Features",
+    href: "#features",
+  },
+];
