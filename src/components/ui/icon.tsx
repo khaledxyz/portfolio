971b1d3739a0960960e737847f9f5624821d65bf
khaledxyz <khaledhalloua@outlook.com>
2025-12-18 10:41:39 +0100

    Update icon.tsx

--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -5,12 +5,18 @@ import { cn } from "@/lib/utils";
 export function Icon({
   icon,
   className,
+  strokeWidth = 2,
   ...props
 }: {
   icon: string;
   className?: string;
+  strokeWidth?: number;
 }) {
   return (
-    <IconifyIcon className={cn("size-5", className)} icon={icon} {...props} />
+    <IconifyIcon
+      className={cn("", `[&>path]:stroke-${strokeWidth}`, className)}
+      icon={icon}
+      {...props}
+    />
   );
 }
