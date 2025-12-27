a295c51129d95c6fcfff75cea67a86130ffb6e9a
khaledxyz <khaledhalloua@outlook.com>
2025-12-27 13:46:38 +0100

    temporarily remove features section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,7 +2,6 @@ import { Fragment } from "react/jsx-runtime";
 
 import { AboutSection } from "./_components/about-section";
 import { CtaSection } from "./_components/cta-section";
-import { FeaturesSection } from "./_components/features-section";
 import { HeroSection } from "./_components/hero-section";
 import { ProjectsSection } from "./_components/projects-section";
 import { TechnologiesSection } from "./_components/technologies-section";
@@ -14,7 +13,7 @@ export default function Page() {
       <AboutSection />
       <ProjectsSection />
       <TechnologiesSection />
-      <FeaturesSection />
+      {/* <FeaturesSection /> */}
       <CtaSection />
     </Fragment>
   );
